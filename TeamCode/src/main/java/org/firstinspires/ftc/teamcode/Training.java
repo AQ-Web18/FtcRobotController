c7b8256be5ad236c12dd71e30b4e51cb04a6b34c
AQ-Web18 <aqeel.m.ansari@gmail.com>
2026-07-15 18:14:54 -0500

    Create Training.java
    just practicing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Training.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Autonomous
+public class Training extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addData("Hello","Aqeel");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
