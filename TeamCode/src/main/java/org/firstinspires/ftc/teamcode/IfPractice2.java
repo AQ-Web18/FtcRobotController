b91d6f9975a610de213b54718e038d64db8f0e6e
AQ-Web18 <aqeel.m.ansari@gmail.com>
2026-07-15 21:35:18 -0500

    Create IfPractice2.java
    If Practice 2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IfPractice2.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class IfPractice2 extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+        double lefty = gamepad1.left_stick_y;
+
+        if (lefty < 0) {
+            telemetry.addData("Left Stick", "is Negative");
+        }
+        else if (lefty > .5) {
+            telemetry.addData("Left Stick", "is greater than 50%");
+        }
+        else if (lefty > 0) {
+            telemetry.addData("Left Stick", "Is greater than 0");
+        }
+        else {
+            telemetry.addData("Left Stick","is Zero");
+        }
+
+    }
+}
+
+/*
+
+AND - && if (LeftY < 8.5 && LeftY > 0) {
+
+|| if (leftY < B || rightY < 0) {
+
+NOT ! if (!clawClosed) {  OR
+
+*/
