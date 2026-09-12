4ab539b4d970dcc03647523291fe06d801e7e457
AQ-Web18 <aqeel.m.ansari@gmail.com>
2026-09-12 15:25:47 -0500

    Update WebcamPractice.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/add_onns/WebcamPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/add_onns/WebcamPractice.java
@@ -1,12 +1,8 @@
-package org.firstinspires.ftc.teamcode.pratice;
+package org.firstinspires.ftc.teamcode.add_onns;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.util.BatteryChecker;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.mechanisms.Webcam;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 
